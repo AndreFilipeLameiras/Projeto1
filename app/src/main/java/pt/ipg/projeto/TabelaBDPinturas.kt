15d871d39db96e0bf125c83d0d2ed65331fc39c4
Lameiras <afplameiras@gmail.com>
2022-06-01 14:06:42 +0100

    Alteraçao do nome da class Pinturas

--- app/src/main/java/pt/ipg/projeto/TabelaBDPinturas.kt
+++ app/src/main/java/pt/ipg/projeto/TabelaBDPinturas.kt
@@ -3,7 +3,7 @@ package pt.ipg.projeto
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
-class Pinturas (val db: SQLiteDatabase) {
+class TabelaBDPinturas (val db: SQLiteDatabase) {
     fun cria(){
         db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $TIPO_PINTURA TEXT NOT NULL, $NOME_PINTURA TEXT NOT NULL) ")
     }
