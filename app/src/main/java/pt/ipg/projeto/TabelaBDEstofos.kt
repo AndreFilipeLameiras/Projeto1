175a3f6853abe85e464931e2f7cfd98521c242c5
Lameiras <afplameiras@gmail.com>
2022-06-01 14:07:10 +0100

    Alteraçao do nome da class Estofos

--- app/src/main/java/pt/ipg/projeto/TabelaBDEstofos.kt
+++ app/src/main/java/pt/ipg/projeto/TabelaBDEstofos.kt
@@ -3,7 +3,7 @@ package pt.ipg.projeto
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
-class Estofos (val db: SQLiteDatabase){
+class TabelaBDEstofos (val db: SQLiteDatabase){
     fun cria(){
         db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $TIPO_ESTOFOS TEXT NOT NULL, $NOME_ESTOFOS TEXT NOT NULL)")
     }
