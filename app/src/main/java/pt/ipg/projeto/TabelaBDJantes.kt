58c456865839c391c9727cbb24ead363fd42ad24
Lameiras <afplameiras@gmail.com>
2022-06-05 12:04:09 +0100

    Criada a classe para representar a tabela Jantes

---
+++ app/src/main/java/pt/ipg/projeto/TabelaBDJantes.kt
@@ -0,0 +1,21 @@
+package pt.ipg.projeto
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDJantes (db: SQLiteDatabase): TabelaBD(db, NOME){
+    override fun cria() {
+        db.execSQL("CREATE TABLE $nome(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $JANTES TEXT NOT NULL, $LARGURA INTEGER NOT NULL, $ALTURA INTEGER NOT NULL, $RAIO INTEGER NOT NULL, $PRECO INTEGER NOT NULL )")
+    }
+
+    companion object{
+        const val NOME = "jantes"
+        const val JANTES = "nome das jantes"
+        const val LARGURA = "largura"
+        const val ALTURA = "altura"
+        const val RAIO = "raio"
+        const val PRECO = "preco"
+
+    }
+
+}
