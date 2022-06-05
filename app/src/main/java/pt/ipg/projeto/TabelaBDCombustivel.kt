24610ab182addd66bbc480f069bcc78537b070e5
Lameiras <afplameiras@gmail.com>
2022-06-05 12:27:07 +0100

    Criada a classe para representar a tabela combustivel

---
+++ app/src/main/java/pt/ipg/projeto/TabelaBDCombustivel.kt
@@ -0,0 +1,17 @@
+package pt.ipg.projeto
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDCombustivel (db: SQLiteDatabase): TabelaBD(db, NOME){
+    override fun cria() {
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL)")
+    }
+
+    companion object{
+        const val NOME = "combustivel"
+        const val CAMPO_NOME = "nome"
+    }
+
+
+}
