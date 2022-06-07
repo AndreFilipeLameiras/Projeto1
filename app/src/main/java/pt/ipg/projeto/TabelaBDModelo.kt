849bf82808498ea7b70801271fa35d7b725cb9f9
Lameiras <afplameiras@gmail.com>
2022-06-07 17:47:29 +0100

    Criada a classe para representar a tabela Modelo

---
+++ app/src/main/java/pt/ipg/projeto/TabelaBDModelo.kt
@@ -0,0 +1,18 @@
+package pt.ipg.projeto
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDModelo (db: SQLiteDatabase): TabelaBD(db, NOME){
+    override fun cria() {
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_MODELO TEXT NOT NULL, $CAMPO_PRECO , $CAMPO_MARCA_ID INTEGER NOT NULL, FOREIGN KEY($CAMPO_MARCA_ID) REFERENCES ${TabelaBDMarcas.NOME})")
+    }
+
+
+    companion object{
+        const val NOME = "modelos"
+        const val CAMPO_MODELO = "nome"
+        const val CAMPO_MARCA_ID = "marcaID"
+        const val CAMPO_PRECO = "preco"
+    }
+}
--- app/src/main/java/pt/ipg/projeto/TabelaBDMotorizacoes.kt
+++ app/src/main/java/pt/ipg/projeto/TabelaBDMotorizacoes.kt
@@ -5,7 +5,7 @@ import android.provider.BaseColumns
 
 class TabelaBDMotorizacoes (db: SQLiteDatabase) : TabelaBD(db, NOME){
     override fun cria(){
-        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_POTENCIA TEXT NOT NULL, $CAMPO_TIPO_DE_CAIXA TEXT NOT NULL, $CAMPO_TIPO_DE_TRACAO TEXT NOT NULL, $CAMPO_TIPO_DE_COMBUSTIVEL TEXT NOT NULL, $CAMPO_CONSUMO INTEGER NOT NULL, $CAMPO_EMISSOES INTEGER NOT NULL)")
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_POTENCIA TEXT NOT NULL, $CAMPO_TRANSMISSOES_ID INTEGER NOT NULL, FOREIGN KEY ($CAMPO_TRANSMISSOES_ID) REFERENCES ${TabelaBDTransmissoes.NOME}, $CAMPO_CONSUMO INTEGER NOT NULL, $CAMPO_EMISSOES INTEGER NOT NULL)")
 
 
     }
@@ -13,9 +13,9 @@ class TabelaBDMotorizacoes (db: SQLiteDatabase) : TabelaBD(db, NOME){
     companion object{
         const val NOME = "motorizacoes"
         const val CAMPO_POTENCIA = "potencia"
-        const val CAMPO_TIPO_DE_CAIXA = "tipo de caixa"
-        const val CAMPO_TIPO_DE_TRACAO = "tipo de tracao"
-        const val CAMPO_TIPO_DE_COMBUSTIVEL = "tipo de combustivel"
+        const val CAMPO_TRANSMISSOES_ID = "transmissoesId"
+        const val CAMPO_TRACAO_ID = "TracaoId"
+        const val CAMPO_COMBUSTIVEL_ID = "combustivelID"
         const val CAMPO_CONSUMO = "consumo"
         const val CAMPO_EMISSOES = "emissoes"
     }
