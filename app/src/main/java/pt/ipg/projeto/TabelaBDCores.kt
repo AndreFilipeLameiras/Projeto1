e34d6dbf2b94738af1f0eb63fbfc43a1c4f84645
Lameiras <afplameiras@gmail.com>
2022-06-09 18:13:12 +0100

    Alteraçao na tabela de cores

---
+++ app/src/main/java/pt/ipg/projeto/TabelaBDCores.kt
@@ -0,0 +1,18 @@
+package pt.ipg.projeto
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDCores (db: SQLiteDatabase): TabelaBD(db, NOME) {
+    override fun cria(){
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, ${TabelaBDPinturas.CAMPO_PINTURA_ID} INTEGER NOT NULL, FOREIGN KEY (${TabelaBDPinturas.CAMPO_PINTURA_ID}) REFERENCES ${TabelaBDPinturas.NOME}, $CAMPO_COR TEXT NOT NULL, $PRECO DOUBLE NOT NULL) ")
+    }
+
+    companion object{
+        const val NOME = "cores"
+        const val CAMPO_PINTURA_ID = "pinturaId"
+        const val CAMPO_COR = "nome"
+        const val PRECO = "preco"
+    }
+
+}
