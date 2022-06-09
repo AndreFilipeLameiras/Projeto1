7a7ce5a4388b60f6c1391a68b6ff44c15a4f10e5
Lameiras <afplameiras@gmail.com>
2022-06-09 18:13:40 +0100

    Criada a classe para representar a tabela Marcas

---
+++ app/src/main/java/pt/ipg/projeto/TabelaBDMarcas.kt
@@ -0,0 +1,18 @@
+package pt.ipg.projeto
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDMarcas (db: SQLiteDatabase): TabelaBD(db, NOME) {
+    override fun cria() {
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $MARCAS TEXT NOT NULL )")
+
+    }
+
+
+    companion object{
+        const val NOME = "marcas"
+        const val MARCAS = "nome"
+
+    }
+}
