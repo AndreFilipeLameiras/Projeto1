b7523edcbf76a1b3c24fde9b8b132bff8a5136b8
Lameiras <afplameiras@gmail.com>
2022-06-15 23:43:26 +0100

    Criaçao funçao para converter uma marca em ContentValues

--- app/src/main/java/pt/ipg/projeto/Marca.kt
+++ app/src/main/java/pt/ipg/projeto/Marca.kt
@@ -1,7 +1,18 @@
 package pt.ipg.projeto
 
+import android.content.ContentValues
+
 data class Marca(
     var id: Long,
     var nome: String
 ) {
+    fun toContentValues(): ContentValues{
+        val valores = ContentValues()
+
+        valores.put(TabelaBDMarcas.MARCAS, nome)
+
+        return valores
+
+    }
+    
 }
