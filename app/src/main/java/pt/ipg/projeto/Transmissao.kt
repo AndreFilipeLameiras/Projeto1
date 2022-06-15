42abe9a0112ceeb3c7c7822b97b52e80ac335d62
Lameiras <afplameiras@gmail.com>
2022-06-15 23:51:40 +0100

    Criaçao funçao para converter uma transmissao em ContentValues

--- app/src/main/java/pt/ipg/projeto/Transmissao.kt
+++ app/src/main/java/pt/ipg/projeto/Transmissao.kt
@@ -1,7 +1,19 @@
 package pt.ipg.projeto
 
+import android.content.ContentValues
+
 data class Transmissao (
     var id: Long,
     var nome: String
         ) {
+
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDTransmissoes.CAMPO_NOME, nome)
+
+
+        return valores
+
+    }
 }
