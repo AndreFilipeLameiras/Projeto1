79cf16bb83c476f304269100260cebb53cf3a88b
Lameiras <afplameiras@gmail.com>
2022-06-15 23:50:56 +0100

    Criaçao funçao para converter uma traçao em ContentValues

--- app/src/main/java/pt/ipg/projeto/Tracao.kt
+++ app/src/main/java/pt/ipg/projeto/Tracao.kt
@@ -1,7 +1,19 @@
 package pt.ipg.projeto
 
+import android.content.ContentValues
+
 data class Tracao (
     var id: Long,
     var nome: String
     ){
+
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDTracao.CAMPO_NOME, nome)
+
+
+        return valores
+
+    }
 }
