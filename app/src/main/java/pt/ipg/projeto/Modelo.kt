b1482d2c51d11a960c303d39ef3a69e370346199
Lameiras <afplameiras@gmail.com>
2022-06-15 23:36:26 +0100

    Criaçao funçao para converter um modelo em ContentValues

--- app/src/main/java/pt/ipg/projeto/Modelo.kt
+++ app/src/main/java/pt/ipg/projeto/Modelo.kt
@@ -1,9 +1,19 @@
 package pt.ipg.projeto
 
+import android.content.ContentValues
+
 data class Modelo(
     var id: Long,
-    var nome: String,
+    var modelo: String,
     var idMarca: Long,
     var preco: Double
 ) {
+    fun toContentValues(): ContentValues{
+        val valores = ContentValues()
+        valores.put(TabelaBDModelo.CAMPO_MODELO, modelo)
+        valores.put(TabelaBDModelo.CAMPO_PRECO, preco)
+        valores.put(TabelaBDModelo.CAMPO_MARCA_ID, idMarca)
+
+        return valores
+    }
 }
