52b95e8bae038f8b557479b63214b5678338a231
Lameiras <afplameiras@gmail.com>
2022-06-15 23:50:02 +0100

    Criaçao funçao para converter uma jante em ContentValues

--- app/src/main/java/pt/ipg/projeto/Jante.kt
+++ app/src/main/java/pt/ipg/projeto/Jante.kt
@@ -1,5 +1,7 @@
 package pt.ipg.projeto
 
+import android.content.ContentValues
+
 data class Jante (
     var id: Long,
     var nome: String,
@@ -8,4 +10,17 @@ data class Jante (
     var raio: Long,
     var preco: Double
     ){
+
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDJantes.JANTES, nome)
+        valores.put(TabelaBDJantes.LARGURA, largura)
+        valores.put(TabelaBDJantes.ALTURA, altura)
+        valores.put(TabelaBDJantes.RAIO, raio)
+        valores.put(TabelaBDJantes.PRECO, preco)
+
+        return valores
+
+    }
 }
