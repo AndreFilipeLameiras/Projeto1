6b5f99263271e26c0fd4d608a2474ec2efaf2590
Lameiras <afplameiras@gmail.com>
2022-06-19 12:01:37 +0100

    Criada funçao para converter um Cursor num Modelo

--- app/src/main/java/pt/ipg/projeto/Modelo.kt
+++ app/src/main/java/pt/ipg/projeto/Modelo.kt
@@ -1,6 +1,8 @@
 package pt.ipg.projeto
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Modelo(
     var modelo: String,
@@ -15,5 +17,22 @@ data class Modelo(
         valores.put(TabelaBDModelo.CAMPO_MARCA_ID, idMarca)
 
         return valores
+    }
+
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Modelo {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posModelo = cursor.getColumnIndex(TabelaBDModelo.CAMPO_MODELO)
+            val posPreco = cursor.getColumnIndex(TabelaBDModelo.CAMPO_PRECO)
+            val posIdMarc = cursor.getColumnIndex(TabelaBDModelo.CAMPO_MARCA_ID)
+
+            val id = cursor.getLong(posId)
+            val modelo = cursor.getString(posModelo)
+            val preco = cursor.getDouble(posPreco)
+            val idMarca = cursor.getLong(posIdMarc)
+
+            return Modelo(modelo, preco, idMarca, id)
+        }
     }
 }
