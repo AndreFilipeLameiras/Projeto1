061c93c2f01040e85b7b5eb69cd2f215ba98fc44
Lameiras <afplameiras@gmail.com>
2022-06-28 14:47:27 +0100

    Criado fragment para mostrar a lista de modelos

---
+++ app/src/main/java/pt/ipg/projeto/ListaModelosfragment.kt
@@ -0,0 +1,35 @@
+package pt.ipg.projeto
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import pt.ipg.projeto.databinding.FragmentListaModelosBinding
+
+class ListaModelosfragment : Fragment(){
+
+    private var _binding: FragmentListaModelosBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+       _binding = FragmentListaModelosBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+
+    }
+}
