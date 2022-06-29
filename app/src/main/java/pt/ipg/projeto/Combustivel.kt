6484e81931c79895cf996bb41eddd0df7f0e3f5c
Lameiras <afplameiras@gmail.com>
2022-06-29 23:12:45 +0100

    Conclusao da classe Combustivel

--- app/src/main/java/pt/ipg/projeto/Combustivel.kt
+++ app/src/main/java/pt/ipg/projeto/Combustivel.kt
@@ -1,6 +1,35 @@
 package pt.ipg.projeto
 
-data class Combustivel (var id: Long, var nome: String){
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
+data class Combustivel (
+    var nome: String,
+    var id: Long = -1
+    ){
+
+
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDCombustivel.CAMPO_NOME, nome)
+
+
+        return valores
+
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor) : Combustivel{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaBDCombustivel.CAMPO_NOME)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+
+            return Combustivel(nome, id)
+        }
+    }
 
 }
