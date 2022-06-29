2bf36e42cad6e6be005e6c37d3154e3cccc0fecf
Lameiras <afplameiras@gmail.com>
2022-06-29 23:03:25 +0100

    Criada funçao para converter um Cursor numa Motorizaçao

--- app/src/main/java/pt/ipg/projeto/Motorizacao.kt
+++ app/src/main/java/pt/ipg/projeto/Motorizacao.kt
@@ -1,15 +1,17 @@
 package pt.ipg.projeto
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Motorizacao (
-    var id: Long,
     var potencia: Long,
+    var consumo: Double,
+    var emissoes: Double,
     var idTransmissoes: Long,
     var idTracao: Long,
     var idCombustivel: Long,
-    var consumo: Double,
-    var emissoes: Double
+    var id: Long = -1
 ){
     fun toContentValues(): ContentValues{
         val valores = ContentValues()
@@ -24,5 +26,29 @@ data class Motorizacao (
         return valores
 
     }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Motorizacao {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posPotencia = cursor.getColumnIndex(TabelaBDMotorizacoes.CAMPO_POTENCIA)
+            val posConsumo = cursor.getColumnIndex(TabelaBDMotorizacoes.CAMPO_CONSUMO)
+            val posEmissoes = cursor.getColumnIndex(TabelaBDMotorizacoes.CAMPO_EMISSOES)
+            val posIdTransmissao = cursor.getColumnIndex(TabelaBDMotorizacoes.CAMPO_TRANSMISSOES_ID)
+            val posIdTracao = cursor.getColumnIndex(TabelaBDMotorizacoes.CAMPO_TRACAO_ID)
+            val posIdCombustivel = cursor.getColumnIndex(TabelaBDMotorizacoes.CAMPO_COMBUSTIVEL_ID)
+
+
+            val id = cursor.getLong(posId)
+            val potencia = cursor.getLong(posPotencia)
+            val consumo = cursor.getDouble(posConsumo)
+            val emissoes = cursor.getDouble(posEmissoes)
+            val idTracao = cursor.getLong(posIdTracao)
+            val idTransmissao = cursor.getLong(posIdTransmissao)
+            val idCombustivel = cursor.getLong(posIdCombustivel)
+
+            return Motorizacao(potencia, consumo, emissoes, idTracao, idTransmissao, idCombustivel, id)
+        }
+    }
+
 
 }
