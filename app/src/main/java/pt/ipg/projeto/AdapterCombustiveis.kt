ef881c7f671ad8c798bbf7d0f712061dd0ad6611
Lameiras <afplameiras@gmail.com>
2022-06-30 12:00:33 +0100

    Configurado o recyclerView para passar os dados dos combustiveis para o AdapterCombustiveis

--- app/src/main/java/pt/ipg/projeto/AdapterCombustiveis.kt
+++ app/src/main/java/pt/ipg/projeto/AdapterCombustiveis.kt
@@ -1,10 +1,11 @@
 package pt.ipg.projeto
 
+import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterCombustiveis: RecyclerView.Adapter<AdapterCombustiveis.ViewHolderCombustiveis>() {
+class AdapterCombustiveis(var cursor: Cursor? = null): RecyclerView.Adapter<AdapterCombustiveis.ViewHolderCombustiveis>() {
     class ViewHolderCombustiveis(itemView: View): RecyclerView.ViewHolder(itemView)
 
     /**
