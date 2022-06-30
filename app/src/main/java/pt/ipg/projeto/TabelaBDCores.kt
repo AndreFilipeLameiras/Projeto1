cd03c77c89d8028359af2e20a2294fe3e5495ebb
Lameiras <afplameiras@gmail.com>
2022-06-30 22:55:48 +0100

    Iniciar um cursorLoader para ler as Cores em background

--- app/src/main/java/pt/ipg/projeto/TabelaBDCores.kt
+++ app/src/main/java/pt/ipg/projeto/TabelaBDCores.kt
@@ -5,14 +5,16 @@ import android.provider.BaseColumns
 
 class TabelaBDCores (db: SQLiteDatabase): TabelaBD(db, NOME) {
     override fun cria(){
-        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_PINTURA_ID INTEGER NOT NULL, FOREIGN KEY ($CAMPO_PINTURA_ID) REFERENCES ${TabelaBDCoresPintura.NOME}, $CAMPO_COR TEXT NOT NULL, $PRECO DOUBLE NOT NULL) ")
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,  $CAMPO_COR TEXT NOT NULL, $PRECO DOUBLE NOT NULL) ")
     }
 
     companion object{
         const val NOME = "cores"
-        const val CAMPO_PINTURA_ID = "pinturaId"
+        //const val CAMPO_PINTURA_ID = "pinturaId"
         const val CAMPO_COR = "nome"
         const val PRECO = "preco"
+
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_COR, PRECO)
     }
 
 }
