dbcc2eb4d365e36560534e09cbdb20feffd5c1d6
Lameiras <afplameiras@gmail.com>
2022-06-30 15:30:04 +0100

    Criado fragment para mostrar lista de transmissoes

---
+++ app/src/main/java/pt/ipg/projeto/ListaTransmissoesFragment.kt
@@ -0,0 +1,36 @@
+package pt.ipg.projeto
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import pt.ipg.projeto.databinding.FragmentListaTransmissaoBinding
+
+class ListaTransmissoesFragment: Fragment() {
+    private var _binding: FragmentListaTransmissaoBinding? = null
+
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentListaTransmissaoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+
+    }
+}
