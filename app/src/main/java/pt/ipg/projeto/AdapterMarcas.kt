d1e76830f328e39c914b436d90d5d96ea0736316
Lameiras <afplameiras@gmail.com>
2022-07-02 12:41:42 +0100

    Configuraçao do recyclerView para passar os dados para o adapter

--- app/src/main/java/pt/ipg/projeto/AdapterMarcas.kt
+++ app/src/main/java/pt/ipg/projeto/AdapterMarcas.kt
@@ -1,10 +1,11 @@
 package pt.ipg.projeto
 
+import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterMarcas: RecyclerView.Adapter<AdapterMarcas.ViewHolderMarcas>() {
+class AdapterMarcas(var cursor: Cursor? = null): RecyclerView.Adapter<AdapterMarcas.ViewHolderMarcas>() {
     class ViewHolderMarcas(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
