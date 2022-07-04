cbfbb3494492abf483271cdaec663307ab984b15
Lameiras <afplameiras@gmail.com>
2022-07-04 15:31:36 +0100

    Implementadas opçoes cancelar e eliminar no fragmento eliminar modelo

--- app/src/main/java/pt/ipg/projeto/EliminarModeloFragment.kt
+++ app/src/main/java/pt/ipg/projeto/EliminarModeloFragment.kt
@@ -1,11 +1,15 @@
 package pt.ipg.projeto
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import pt.ipg.projeto.databinding.FragmentEliminarModeloBinding
 
 
@@ -49,14 +53,42 @@ class EliminarModeloFragment : Fragment() {
     fun processaOpcaoMenu(item: MenuItem) : Boolean =
         when(item.itemId){
             R.id.action_eliminar -> {
+                eliminaModelo()
                 true
             }
             R.id.action_cancelar -> {
-
+                voltaListaModelos()
                 true
             }
             else -> false
         }
+
+    private fun eliminaModelo() {
+        val enderecoModelo = Uri.withAppendedPath(ContentProviderCarros.ENDERECO_MODELOS, "${modelo.id}")
+        val registosEliminados = requireActivity().contentResolver.delete(enderecoModelo, null, null)
+
+        if(registosEliminados != 1){
+            Snackbar.make(
+                binding,textViewModelo,
+                R.string.erro_eliminar_modelo,
+                Snackbar.LENGTH_INDEFINITE
+            ).show()
+            return
+        }
+
+        Toast.makeText(requireContext(), R.string.modelo_eliminado_sucesso, Toast.LENGTH_LONG).show()
+        voltaListaModelos()
+
+
+    }
+
+
+    private fun voltaListaModelos() {
+        val acao = EliminarModeloFragmentDirections.actionEliminarModeloFragmentToListaModelosfragment()
+        findNavController().navigate(acao)
+    }
+
+
 
 
 }
