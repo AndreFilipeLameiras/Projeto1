ae99683b34242a2f902cf16a4ecb063603f50b51
Lameiras <afplameiras@gmail.com>
2022-07-04 14:36:47 +0100

    Ao clicar na opçao eliminar na lista de modelos mostra o fragmento eliminar modelo

--- app/src/main/java/pt/ipg/projeto/ListaModelosfragment.kt
+++ app/src/main/java/pt/ipg/projeto/ListaModelosfragment.kt
@@ -148,11 +148,16 @@ class ListaModelosfragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>{
     fun processaOpcaoMenu(item: MenuItem) : Boolean =
         when(item.itemId){
             R.id.action_inserir -> {
-                findNavController().navigate(R.id.action_listaMarcasFragment_to_inserirMarcaFragment)
+                val acao = ListaModelosFragmentDirections.ListaModelosfragmentDirections.actionListaModelosfragmentToInserirModeloFragment()
+                findNavController().navigate(acao)
                 true
             }
             R.id.action_alterar -> true
-            R.id.action_eliminar -> true
+            R.id.action_eliminar -> {
+                val acao = ListaModelosFragmentDirections.actionListaModelosfragmentToEliminarModeloFragment(modeloSeleccionado!!)
+                findNavController().navigate(acao)
+                true
+            }
             else -> false
         }
 
