5c1e84c7103124911f0650b46ff3bbadec31e325
Lameiras <afplameiras@gmail.com>
2022-07-10 23:52:55 +0100

    retirado o floatingActionButton

--- app/src/main/java/pt/ipg/projeto/MainActivity.kt
+++ app/src/main/java/pt/ipg/projeto/MainActivity.kt
@@ -31,6 +31,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    var janteSeleccionada : Jante? = null
+
     var menu: Menu? = null
 
     var fragment: Fragment? = null
@@ -43,10 +45,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
+        /*binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -91,6 +93,8 @@ class MainActivity : AppCompatActivity() {
             opcaoProcessada = (fragment as ListaJantesFragment).processaOpcaoMenu(item)
         } else if (fragment is InserirJantesFragment) {
             opcaoProcessada = (fragment as InserirJantesFragment).processaOpcaoMenu(item)
+        } else if (fragment is EditaJanteFragment) {
+            opcaoProcessada = (fragment as EditaJanteFragment).processaOpcaoMenu(item)
         } else {
             opcaoProcessada = false
         }
