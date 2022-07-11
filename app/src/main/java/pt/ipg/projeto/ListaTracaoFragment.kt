dea72fbcecfef96ac2bd1009e324a9cfba510417
Lameiras <afplameiras@gmail.com>
2022-07-11 19:38:51 +0100

    Implementar a opçao inserir traçao

--- app/src/main/java/pt/ipg/projeto/ListaTracaoFragment.kt
+++ app/src/main/java/pt/ipg/projeto/ListaTracaoFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import pt.ipg.projeto.databinding.FragmentListaTracaoBinding
@@ -137,7 +138,10 @@ class ListaTracaoFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>{
 
     fun processaOpcaoMenu(item: MenuItem): Boolean =
         when(item.itemId){
-            R.id.action_inserir -> true
+            R.id.action_inserir -> {
+                findNavController().navigate(R.id.action_listaTracaoFragment_to_inserirTracaoFragment)
+                true
+            }
             R.id.action_alterar -> true
             R.id.action_eliminar -> true
             else -> false
