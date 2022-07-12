5b7e232e82e5cc27519793156f174e6658b19552
Lameiras <afplameiras@gmail.com>
2022-07-12 01:27:00 +0100

    Adicionado menu para ediçao e implementar cancelar(nos fragmentos Inserir Combustivel e Transmissao)

--- app/src/main/java/pt/ipg/projeto/InserirCombustivelFragment.kt
+++ app/src/main/java/pt/ipg/projeto/InserirCombustivelFragment.kt
@@ -3,8 +3,10 @@ package pt.ipg.projeto
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import pt.ipg.projeto.databinding.FragmentInserirCombustivelBinding
 
 
@@ -28,6 +30,26 @@ class InserirCombustivelFragment : Fragment() {
 
         return inflater.inflate(R.layout.fragment_inserir_combustivel, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_edicao
+    }
+
+    fun processaOpcaoMenu(item: MenuItem) : Boolean =
+        when(item.itemId) {
+            R.id.action_guardar -> {
+                true
+            }
+            R.id.action_cancelar -> {
+                findNavController().navigate(R.id.action_inserirCombustivelFragment_to_listaCombustivelFragment)
+                true
+            }
+            else -> false
+        }
 
 
 }
--- app/src/main/java/pt/ipg/projeto/InserirTransmissaoFragment.kt
+++ app/src/main/java/pt/ipg/projeto/InserirTransmissaoFragment.kt
@@ -3,8 +3,11 @@ package pt.ipg.projeto
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.navigation.fragment.findNavController
 import pt.ipg.projeto.databinding.FragmentInserirTransmissaoBinding
 
 
@@ -28,6 +31,27 @@ class InserirTransmissaoFragment : Fragment() {
 
         return inflater.inflate(R.layout.fragment_inserir_transmissao, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_edicao
+    }
+
+
+    fun processaOpcaoMenu(item: MenuItem) : Boolean =
+        when(item.itemId) {
+            R.id.action_guardar -> {
+                true
+            }
+            R.id.action_cancelar -> {
+                findNavController().navigate(R.id.action_inserirTransmissaoFragment_to_listaTransmissoesFragment)
+                true
+            }
+            else -> false
+        }
 
 
 }
