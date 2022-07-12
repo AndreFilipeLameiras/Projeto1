2e169e5198181421acecdb3e84d0e6a952874eda
Lameiras <afplameiras@gmail.com>
2022-07-12 23:32:07 +0100

    Criada para converter um carro em ContentValues

--- app/src/main/java/pt/ipg/projeto/Carro.kt
+++ app/src/main/java/pt/ipg/projeto/Carro.kt
@@ -1,5 +1,7 @@
 package pt.ipg.projeto
 
+import android.content.ContentValues
+
 data class Carro (
     var id: Long,
     var idModelo: Long,
@@ -7,7 +9,19 @@ data class Carro (
     var idPintura: Long,
     var idEstofos: Long,
     var idJantes: Long
+){
+    fun toContentValues(): ContentValues{
+        val valores = ContentValues()
+
+        valores.put(TabelaBDCarros.CAMPO_MODELO_ID, idModelo )
+        valores.put(TabelaBDCarros.CAMPO_MOTORIZACAO_ID, idMotorizacao )
+        valores.put(TabelaBDCarros.CAMPO_PINTURA_ID, idPintura )
+        valores.put(TabelaBDCarros.CAMPO_ESTOFOS_ID, idEstofos)
+        valores.put(TabelaBDCarros.CAMPO_JANTES_ID, idJantes)
+
+        return valores
+    }
 
 
-    ){
+
 }
