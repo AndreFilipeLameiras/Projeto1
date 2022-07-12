fa65847b0088fdbfdb11f3ef19147494c153064c
Lameiras <afplameiras@gmail.com>
2022-07-12 11:04:19 +0100

    perguntar ao utilizador se pretende eliminar

--- app/src/main/java/pt/ipg/projeto/EliminarCombustivelFragment.kt
+++ app/src/main/java/pt/ipg/projeto/EliminarCombustivelFragment.kt
@@ -1,11 +1,17 @@
 package pt.ipg.projeto
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import pt.ipg.projeto.databinding.FragmentEliminarCombustivelBinding
 
 
@@ -23,19 +29,21 @@ class EliminarCombustivelFragment : Fragment() {
 
     private lateinit var combustivel: Combustivel
 
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentEliminarCombustivelBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -54,15 +62,50 @@ class EliminarCombustivelFragment : Fragment() {
     fun processaOpcaoMenu(item: MenuItem): Boolean =
         when(item.itemId){
             R.id.action_eliminar -> {
-
+                eliminaCombustivel()
                 true
             }
-            R.id.action_cancelar ->{
-
+            R.id.action_cancelar -> {
+                navegaListaCombustivel()
                 true
             }
-            else -> false
         }
+
+
+
+    private fun eliminaCombustivel() {
+        val alertDialog = AlertDialog.Builder(requireContext())
+
+        alertDialog.apply {
+            setTitle(R.string.eliminar_combustivel_label)
+            setMessage(R.string.confirma_eliminar_combustivel)
+            setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener { dialogInterface, i ->  })
+            setPositiveButton(R.string.eliminar, DialogInterface.OnClickListener { dialogInterface, i -> confirmaEliminarCombustivel() })
+        }
+    }
+
+    private fun confirmaEliminarCombustivel(){
+        val enderecoCombustivel = Uri.withAppendedPath(ContentProviderCarros.ENDERECO_COMBUSTIVEIS, "${combustivel.id}")
+        val registosEliminados = requireActivity().contentResolver.delete(enderecoCombustivel, null,null)
+
+        if(registosEliminados != 1){
+            Snackbar.make(
+                binding.textViewNomeCombustivel,
+                R.string.erro_eliminar_combustivel,
+                Snackbar.LENGTH_INDEFINITE
+            ).show()
+            return
+        }
+
+        Toast.makeText(requireContext(), R.string.combustivel_eliminado_sucesso, Toast.LENGTH_LONG).show()
+        navegaListaCombustivel()
+    }
+
+
+    private fun navegaListaCombustivel() {
+        val acao = EliminarCombustivelFragmentDirections.actionEliminarCombustivelFragmentToListaCombustivelFragment()
+        findNavController().navigate(acao)
+    }
 
 
 }
--- app/src/main/java/pt/ipg/projeto/EliminarTransmisssaoFragment.kt
+++ app/src/main/java/pt/ipg/projeto/EliminarTransmisssaoFragment.kt
@@ -1,11 +1,17 @@
 package pt.ipg.projeto
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import pt.ipg.projeto.databinding.FragmentEliminarTransmisssaoBinding
 
 
@@ -55,14 +61,47 @@ class EliminarTransmisssaoFragment : Fragment() {
     fun processaOpcaoMenu(item: MenuItem): Boolean =
         when(item.itemId){
             R.id.action_eliminar -> {
-
+                eliminaTransmissao()
                 true
             }
             R.id.action_cancelar ->{
-
+                navegaListaTransmissao()
                 true
             }
             else -> false
         }
+
+    private fun eliminaTransmissao() {
+        val alertDialog = AlertDialog.Builder(requireContext())
+
+        alertDialog.apply {
+            setTitle(R.string.eliminar_transmissao_label)
+            setMessage(R.string.confirma_eliminar_transmissao)
+            setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener { dialogInterface, i ->  })
+            setPositiveButton(R.string.eliminar, DialogInterface.OnClickListener { dialogInterface, i -> confirmaEliminarTransmissao() })
+        }
+
+    }
+    private fun confirmaEliminarTransmissao(){
+        val enderecoTransmissao = Uri.withAppendedPath(ContentProviderCarros.ENDERECO_TRASNMISSOES, "${transmissao.id}")
+        val registosEliminados = requireActivity().contentResolver.delete(enderecoTransmissao, null, null)
+
+        if(registosEliminados != 1){
+            Snackbar.make(
+                binding.textViewNomeTransmissao,
+                R.string.erro_eliminar_transmissao,
+                Snackbar.LENGTH_INDEFINITE
+            ).show()
+            return
+        }
+
+        Toast.makeText(requireContext(), R.string.transmissao_eliminada_sucesso, Toast.LENGTH_LONG).show()
+        navegaListaTransmissao()
+    }
+
+    private fun navegaListaTransmissao() {
+        val acao = EliminarTransmisssaoFragmentDirections.actionEliminarTransmisssaoFragmentToListaTransmissoesFragment()
+        findNavController().navigate(acao)
+    }
 
 }
