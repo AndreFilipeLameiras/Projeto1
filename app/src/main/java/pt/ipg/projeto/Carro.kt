4a36f0a488361a9c76ac59d40fb732b61af1fd5a
Lameiras <afplameiras@gmail.com>
2022-07-12 23:28:08 +0100

    Criada a classe para representar um carro

---
+++ app/src/main/java/pt/ipg/projeto/Carro.kt
@@ -0,0 +1,13 @@
+package pt.ipg.projeto
+
+data class Carro (
+    var id: Long,
+    var idModelo: Long,
+    var idMotorizacao: Long,
+    var idPintura: Long,
+    var idEstofos: Long,
+    var idJantes: Long
+
+
+    ){
+}
