1f4ef53cf75834b379d4a804d04b4d7d893d49b1
Lameiras <afplameiras@gmail.com>
2022-07-16 14:10:31 +0100

    Criada funçao para converter um Cursor num Carro

--- app/src/main/java/pt/ipg/projeto/Carro.kt
+++ app/src/main/java/pt/ipg/projeto/Carro.kt
@@ -1,6 +1,8 @@
 package pt.ipg.projeto
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Carro (
     var idModelo: Long,
@@ -20,6 +22,27 @@ data class Carro (
         valores.put(TabelaBDCarros.CAMPO_JANTES_ID, idJantes)
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor):Carro{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posIdModelo = cursor.getColumnIndex(TabelaBDCarros.CAMPO_MODELO_ID)
+            val posIdMotorizacao = cursor.getColumnIndex(TabelaBDCarros.CAMPO_MOTORIZACAO_ID)
+            val posIdCor = cursor.getColumnIndex(TabelaBDCarros.CAMPO_COR_ID)
+            val posIdEstofos = cursor.getColumnIndex(TabelaBDCarros.CAMPO_ESTOFOS_ID)
+            val posIdJantes = cursor.getColumnIndex(TabelaBDCarros.CAMPO_JANTES_ID)
+
+            val id = cursor.getLong(posId)
+            val idModelo = cursor.getLong(posIdModelo)
+            val idMotorizacao = cursor.getLong(posIdMotorizacao)
+            val idCor = cursor.getLong(posIdCor)
+            val idEstofos = cursor.getLong(posIdEstofos)
+            val idJantes = cursor.getLong(posIdJantes)
+
+            return Carro(idModelo, idMotorizacao, idCor, idEstofos, idJantes, id)
+
+        }
     }
 
 
