67416e224fbf0ce24d365c7080d93454030a6ee5
Lameiras <afplameiras@gmail.com>
2022-07-17 00:50:04 +0100

    Ao clicar na opçao elminar mostrar o fragmento eliminar carro

--- app/src/main/java/pt/ipg/projeto/ListaCarrosFragment.kt
+++ app/src/main/java/pt/ipg/projeto/ListaCarrosFragment.kt
@@ -152,11 +152,16 @@ class ListaCarrosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     fun processaOpcaoMenu(item: MenuItem) : Boolean =
         when(item.itemId) {
             R.id.action_inserir -> {
-                findNavController().navigate(R.id.action_listaCarrosFragment_to_inserirCarroFragment)
+                val acao = ListaCarrosFragmentDirections.actionListaCarrosFragmentToInserirCarroFragment()
+                findNavController().navigate(acao)
                 true
             }
             R.id.action_alterar -> true
-            R.id.action_eliminar -> true
+            R.id.action_eliminar -> {
+                val acao = ListaCarrosFragmentDirections.actionListaCarrosFragmentToEliminarCarroFragment()
+                findNavController().navigate(acao)
+                true
+            }
             else -> false
         }
 
